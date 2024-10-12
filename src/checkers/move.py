55063a988cfdf25e46c218bd0b34282c629e92b4
FranChesK0 <khl_doss@mail.ru>
2024-10-12 22:10:19 +0800

    fix wrong comparison of positions and moves

--- src/checkers/move.py
+++ src/checkers/move.py
@@ -1,13 +1,21 @@
-import dataclasses
-
 from .position import Position
 
 
-@dataclasses.dataclass()
 class Move:
     def __init__(self, from_: Position = Position(), to: Position = Position()) -> None:
         self.__from = from_
         self.__to = to
+
+    def __repr__(self) -> str:
+        return f"{self.from_.x}:{self.from_.y} -> {self.to.x}:{self.to.y}"
+
+    def __str__(self) -> str:
+        return self.__repr__()
+
+    def __eq__(self, value: object) -> bool:
+        if not isinstance(value, Move):
+            return NotImplemented
+        return self.from_ == value.from_ and self.to == value.to
 
     @property
     def from_(self) -> Position:
@@ -16,9 +24,3 @@ class Move:
     @property
     def to(self) -> Position:
         return self.__to
-
-    def __repr__(self) -> str:
-        return f"{self.from_.x}:{self.from_.y} -> {self.to.x}:{self.to.y}"
-
-    def __str__(self) -> str:
-        return self.__repr__()
--- src/checkers/position.py
+++ src/checkers/position.py
@@ -1,12 +1,15 @@
-import dataclasses
 from typing import Tuple
 
 
-@dataclasses.dataclass()
 class Position:
     def __init__(self, x: int = -1, y: int = -1) -> None:
         self.__x = x
         self.__y = y
+
+    def __eq__(self, value: object) -> bool:
+        if not isinstance(value, Position):
+            return NotImplemented
+        return self.x == value.x and self.y == value.y
 
     @property
     def x(self) -> int:
